e955359b025516c2fafabdef369e206a21da28d0
Nikhil Tomar <nikhiltomarsan2712@gmail.com>
2026-07-06 07:47:58 +0530

    Time: 14 ms (77.81%), Space: 56.8 MB (85.6%) - LeetHub

---
+++ 0018-4sum/0018-4sum.js
@@ -0,0 +1,45 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[][]}
+ */
+var fourSum = function(nums, target) {
+    const result = [];
+    nums.sort((a, b) => a - b);
+    
+    for (let i = 0; i < nums.length - 3; i++) {
+        // Skip duplicates for i
+        if (i > 0 && nums[i] === nums[i - 1]) continue;
+        
+        for (let j = i + 1; j < nums.length - 2; j++) {
+            // Skip duplicates for j
+            if (j > i + 1 && nums[j] === nums[j - 1]) continue;
+            
+            let left = j + 1;
+            let right = nums.length - 1;
+            
+            while (left < right) {
+                const sum = nums[i] + nums[j] + nums[left] + nums[right];
+                
+                if (sum === target) {
+                    result.push([nums[i], nums[j], nums[left], nums[right]]);
+                    
+                    // Skip duplicates
+                    while (left < right && nums[left] === nums[left + 1]) left++;
+                    while (left < right && nums[right] === nums[right - 1]) right--;
+                    
+                    left++;
+                    right--;
+                } 
+                else if (sum < target) {
+                    left++;
+                } 
+                else {
+                    right--;
+                }
+            }
+        }
+    }
+    
+    return result;
+};
