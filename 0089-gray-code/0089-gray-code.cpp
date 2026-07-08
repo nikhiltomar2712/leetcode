14f59a88dc2d387d006af4759129f9200c4513e7
Nikhil Tomar <nikhiltomarsan2712@gmail.com>
2026-07-08 08:37:43 +0530

    Time: 0 ms (100%), Space: 14.6 MB (49.89%) - LeetHub

---
+++ 0089-gray-code/0089-gray-code.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> grayCode(int n) {
+        vector<int> result;
+        int size = 1 << n; // 2^n
+        for (int i = 0; i < size; ++i) {
+            result.push_back(i ^ (i >> 1));
+        }
+        return result;
+    }
+};
