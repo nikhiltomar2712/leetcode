d36c41721977cd30b3829a7559fa0d9d68aca0f2
Nikhil Tomar <nikhiltomarsan2712@gmail.com>
2026-07-08 08:23:22 +0530

    Time: 3 ms (63.94%), Space: 11.5 MB (73.51%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<vector<int>> result;
+        sort(nums.begin(), nums.end());
+        vector<bool> used(nums.size(), false);
+        vector<int> current;
+        backtrack(nums, used, current, result);
+        return result;
+    }
+    
+private:
+    void backtrack(vector<int>& nums, vector<bool>& used, vector<int>& current, vector<vector<int>>& result) {
+        if (current.size() == nums.size()) {
+            result.push_back(current);
+            return;
+        }
+        for (int i = 0; i < nums.size(); ++i) {
+            if (used[i]) continue;
+            // Skip duplicate numbers at the same depth
+            if (i > 0 && nums[i] == nums[i-1] && !used[i-1]) continue;
+            used[i] = true;
+            current.push_back(nums[i]);
+            backtrack(nums, used, current, result);
+            current.pop_back();
+            used[i] = false;
+        }
+    }
+};
