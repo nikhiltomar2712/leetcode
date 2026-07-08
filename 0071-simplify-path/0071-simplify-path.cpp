d42c5bc2ba53103e3f78b068fd7c514f2b74938a
Nikhil Tomar <nikhiltomarsan2712@gmail.com>
2026-07-08 08:32:02 +0530

    Time: 0 ms (100%), Space: 10.8 MB (88.43%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <vector>
+#include <sstream>
+using namespace std;
+
+class Solution {
+public:
+    string simplifyPath(string path) {
+        vector<string> stack;
+        stringstream ss(path);
+        string token;
+        while (getline(ss, token, '/')) {
+            if (token.empty() || token == ".") {
+                continue;
+            } else if (token == "..") {
+                if (!stack.empty()) {
+                    stack.pop_back();
+                }
+            } else {
+                stack.push_back(token);
+            }
+        }
+        string result = "/";
+        for (int i = 0; i < stack.size(); ++i) {
+            result += stack[i];
+            if (i < stack.size() - 1) {
+                result += "/";
+            }
+        }
+        return result;
+    }
+};
