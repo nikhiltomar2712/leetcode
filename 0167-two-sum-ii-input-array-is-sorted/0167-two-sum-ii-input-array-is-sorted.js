eae20269a3ac262a14b36d3983340b60906aab5e
Nikhil Tomar <nikhiltomarsan2712@gmail.com>
2026-07-10 07:59:13 +0530

    Time: 1 ms (84.7%), Space: 54.3 MB (94.18%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    let left = 0;
+    let right = numbers.length - 1;
+    
+    while (left < right) {
+        const sum = numbers[left] + numbers[right];
+        
+        if (sum === target) {
+            return [left + 1, right + 1]; // 1-indexed
+        } else if (sum < target) {
+            left++; // Need larger sum, move left pointer right
+        } else {
+            right--; // Need smaller sum, move right pointer left
+        }
+    }
+    
+    return []; // Should never reach here as solution is guaranteed
+};
