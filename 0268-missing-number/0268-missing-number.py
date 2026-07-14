4b485012efad98af4c448e996eb9587872b30367
Nikhil Tomar <nikhiltomarsan2712@gmail.com>
2026-07-14 17:48:23 +0530

    Time: 4 ms (49.21%), Space: 13.4 MB (35.34%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        n = len(nums)
+        missing = n
+        
+        for i in range(n):
+            missing ^= i ^ nums[i]
+        
+        return missing
