cedf7b59f4a8d97aa23ae86fc4bd9846ef63f397
Nikhil Tomar <nikhiltomarsan2712@gmail.com>
2026-07-25 15:54:56 +0530

    Time: 4 ms (100%), Space: 46.5 MB (31.97%) - LeetHub

---
+++ 0641-design-circular-deque/0641-design-circular-deque.java
@@ -0,0 +1,61 @@
+class MyCircularDeque {
+    private int[] data;
+    private int front;
+    private int rear;
+    private int size;
+    private int capacity;
+
+    public MyCircularDeque(int k) {
+        capacity = k;
+        data = new int[k];
+        front = 0;
+        rear = 0;
+        size = 0;
+    }
+
+    public boolean insertFront(int value) {
+        if (isFull()) return false;
+        front = (front - 1 + capacity) % capacity;
+        data[front] = value;
+        size++;
+        return true;
+    }
+
+    public boolean insertLast(int value) {
+        if (isFull()) return false;
+        data[rear] = value;
+        rear = (rear + 1) % capacity;
+        size++;
+        return true;
+    }
+
+    public boolean deleteFront() {
+        if (isEmpty()) return false;
+        front = (front + 1) % capacity;
+        size--;
+        return true;
+    }
+
+    public boolean deleteLast() {
+        if (isEmpty()) return false;
+        rear = (rear - 1 + capacity) % capacity;
+        size--;
+        return true;
+    }
+
+    public int getFront() {
+        return isEmpty() ? -1 : data[front];
+    }
+
+    public int getRear() {
+        return isEmpty() ? -1 : data[(rear - 1 + capacity) % capacity];
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public boolean isFull() {
+        return size == capacity;
+    }
+}
