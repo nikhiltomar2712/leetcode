596cc485099a85d08049e151202b6b1e704d0890
Nikhil Tomar <nikhiltomarsan2712@gmail.com>
2026-08-06 06:27:13 +0530

    Time: 6 ms (90.02%), Space: 45.1 MB (98.91%) - LeetHub

---
+++ 0812-largest-triangle-area/0812-largest-triangle-area.java
@@ -0,0 +1,27 @@
+class Solution {
+    public double largestTriangleArea(int[][] points) {
+        int n = points.length;
+        double maxArea = 0.0;
+        
+        // Try all combinations of three points
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = i + 1; j < n - 1; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    double area = calculateArea(points[i], points[j], points[k]);
+                    maxArea = Math.max(maxArea, area);
+                }
+            }
+        }
+        
+        return maxArea;
+    }
+    
+    private double calculateArea(int[] p1, int[] p2, int[] p3) {
+        // Shoelace formula: Area = 0.5 * |x1(y2 - y3) + x2(y3 - y1) + x3(y1 - y2)|
+        return 0.5 * Math.abs(
+            p1[0] * (p2[1] - p3[1]) +
+            p2[0] * (p3[1] - p1[1]) +
+            p3[0] * (p1[1] - p2[1])
+        );
+    }
+}
