f7f7bba347234b40b82d6a4580cfc43b4bb17d3d
Nikhil Tomar <nikhiltomarsan2712@gmail.com>
2026-08-16 11:52:18 +0530

    Time: 4 ms (75.68%), Space: 114.7 MB (64.86%) - LeetHub

---
+++ 2029-stone-game-ix/2029-stone-game-ix.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean stoneGameIX(int[] stones) {
+        int[] count = new int[3];
+        for (int stone : stones) {
+            count[stone % 3]++;
+        }
+        
+        // If count of stones with remainder 0 is even
+        if (count[0] % 2 == 0) {
+            return Math.min(count[1], count[2]) > 0;
+        }
+        // If count of stones with remainder 0 is odd
+        return Math.abs(count[1] - count[2]) > 2;
+    }
+}
