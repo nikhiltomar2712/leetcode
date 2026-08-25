bf944f7ac80855479457cc2ef617d1ef5c1e7a7b
Nikhil Tomar <nikhiltomarsan2712@gmail.com>
2026-08-25 09:07:18 +0530

    Time: 382 ms (77.81%), Space: 0B (100%) - LeetHub

---
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+SELECT DISTINCT 
+    email
+FROM 
+    Person
+WHERE 
+    email IN (
+        SELECT email 
+        FROM Person 
+        GROUP BY email 
+        HAVING COUNT(*) > 1
+    );
