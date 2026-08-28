a83d3a4bb68f45e6931aacb9280998dfe2fe1bdc
Nikhil Tomar <nikhiltomarsan2712@gmail.com>
2026-08-28 06:35:50 +0530

    Time: 0 ms (100%), Space: 42 MB (72.34%) - LeetHub

---
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int bitwiseComplement(int n) {
+        if (n == 0) return 1;
+        
+        int result = 0;
+        int i = 0;
+        while (n > 0) {
+            // Flip the least significant bit and place it
+            result |= ((n & 1) ^ 1) << i;
+            n >>= 1;
+            i++;
+        }
+        return result;
+    }
+}
