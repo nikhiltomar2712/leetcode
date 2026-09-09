de2c9310156152319d984d48df23252f118a6c23
Nikhil Tomar <nikhiltomarsan2712@gmail.com>
2026-09-09 08:25:39 +0530

    Time: 21 ms (88.27%), Space: 48.9 MB (69.39%) - LeetHub

---
+++ 1261-find-elements-in-a-contaminated-binary-tree/1261-find-elements-in-a-contaminated-binary-tree.java
@@ -0,0 +1,20 @@
+class FindElements {
+    private Set<Integer> values;
+
+    public FindElements(TreeNode root) {
+        values = new HashSet<>();
+        recover(root, 0);
+    }
+
+    private void recover(TreeNode node, int val) {
+        if (node == null) return;
+        node.val = val;
+        values.add(val);
+        recover(node.left, 2 * val + 1);
+        recover(node.right, 2 * val + 2);
+    }
+
+    public boolean find(int target) {
+        return values.contains(target);
+    }
+}
