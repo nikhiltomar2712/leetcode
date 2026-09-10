d58b6d7777a4c4970d6456d14446ed4361958b8d
Nikhil Tomar <nikhiltomarsan2712@gmail.com>
2026-09-10 14:23:00 +0530

    Time: 18 ms (44.95%), Space: 48.8 MB (53.51%) - LeetHub

---
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+        
+        inorder(root1, list1);
+        inorder(root2, list2);
+        
+        return merge(list1, list2);
+    }
+    
+    private void inorder(TreeNode node, List<Integer> list) {
+        if (node == null) return;
+        inorder(node.left, list);
+        list.add(node.val);
+        inorder(node.right, list);
+    }
+    
+    private List<Integer> merge(List<Integer> a, List<Integer> b) {
+        List<Integer> result = new ArrayList<>();
+        int i = 0, j = 0;
+        while (i < a.size() && j < b.size()) {
+            if (a.get(i) <= b.get(j)) {
+                result.add(a.get(i++));
+            } else {
+                result.add(b.get(j++));
+            }
+        }
+        while (i < a.size()) result.add(a.get(i++));
+        while (j < b.size()) result.add(b.get(j++));
+        return result;
+    }
+}
