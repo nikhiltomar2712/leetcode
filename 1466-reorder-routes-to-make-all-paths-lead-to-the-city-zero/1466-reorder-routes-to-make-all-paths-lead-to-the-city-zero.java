f39b6ab2388be463e5e9e0245732fed8a57b8745
Nikhil Tomar <nikhiltomarsan2712@gmail.com>
2026-09-17 19:50:30 +0530

    Time: 37 ms (65.86%), Space: 115.7 MB (46.66%) - LeetHub

---
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int minReorder(int n, int[][] connections) {
+        List<List<int[]>> graph = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int[] c : connections) {
+            graph.get(c[0]).add(new int[]{c[1], 1});
+            graph.get(c[1]).add(new int[]{c[0], 0});
+        }
+
+        boolean[] visited = new boolean[n];
+        return dfs(0, graph, visited);
+    }
+
+    private int dfs(int node, List<List<int[]>> graph, boolean[] visited) {
+        visited[node] = true;
+        int changes = 0;
+
+        for (int[] neighbor : graph.get(node)) {
+            int next = neighbor[0];
+            int cost = neighbor[1];
+            if (!visited[next]) {
+                changes += cost + dfs(next, graph, visited);
+            }
+        }
+
+        return changes;
+    }
+}
