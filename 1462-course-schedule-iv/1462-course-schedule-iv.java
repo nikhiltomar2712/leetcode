1fcbddf19e50f6dfbfa8bff493569f34c9566e18
Nikhil Tomar <nikhiltomarsan2712@gmail.com>
2026-09-17 19:43:11 +0530

    Time: 24 ms (26.12%), Space: 45.3 MB (94.61%) - LeetHub

---
+++ 1462-course-schedule-iv/1462-course-schedule-iv.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<Boolean> checkIfPrerequisite(int numCourses, int[][] prerequisites, int[][] queries) {
+        boolean[][] reachable = new boolean[numCourses][numCourses];
+
+        for (int[] pre : prerequisites) {
+            reachable[pre[0]][pre[1]] = true;
+        }
+
+        for (int k = 0; k < numCourses; k++) {
+            for (int i = 0; i < numCourses; i++) {
+                for (int j = 0; j < numCourses; j++) {
+                    if (reachable[i][k] && reachable[k][j]) {
+                        reachable[i][j] = true;
+                    }
+                }
+            }
+        }
+
+        List<Boolean> result = new ArrayList<>();
+        for (int[] q : queries) {
+            result.add(reachable[q[0]][q[1]]);
+        }
+
+        return result;
+    }
+}
