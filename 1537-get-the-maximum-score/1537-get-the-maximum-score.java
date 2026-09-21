0002d41c7b275ac1efdd34862403acd3c93e7174
Nikhil Tomar <nikhiltomarsan2712@gmail.com>
2026-09-21 18:09:38 +0530

    Time: 4 ms (99.59%), Space: 61.4 MB (64.46%) - LeetHub

---
+++ 1537-get-the-maximum-score/1537-get-the-maximum-score.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxSum(int[] nums1, int[] nums2) {
+        int MOD = 1_000_000_007;
+        int i = 0, j = 0;
+        long sum1 = 0, sum2 = 0;
+
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] < nums2[j]) {
+                sum1 += nums1[i++];
+            } else if (nums1[i] > nums2[j]) {
+                sum2 += nums2[j++];
+            } else {
+                sum1 = sum2 = Math.max(sum1, sum2) + nums1[i];
+                i++;
+                j++;
+            }
+        }
+
+        while (i < nums1.length) sum1 += nums1[i++];
+        while (j < nums2.length) sum2 += nums2[j++];
+
+        return (int) (Math.max(sum1, sum2) % MOD);
+    }
+}
