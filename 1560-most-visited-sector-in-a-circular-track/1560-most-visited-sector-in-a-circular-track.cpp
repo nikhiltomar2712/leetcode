ddbffac76470e89b2a983d13c4ebc204bbcfea43
Nikhil Tomar <nikhiltomarsan2712@gmail.com>
2026-09-22 19:26:08 +0530

    Time: 0 ms (100%), Space: 15 MB (86.6%) - LeetHub

---
+++ 1560-most-visited-sector-in-a-circular-track/1560-most-visited-sector-in-a-circular-track.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> mostVisited(int n, vector<int>& rounds) {
+        int start = rounds[0];
+        int end = rounds.back();
+        vector<int> ans;
+        
+        if (start <= end) {
+            for (int i = start; i <= end; ++i)
+                ans.push_back(i);
+        } else {
+            for (int i = 1; i <= end; ++i)
+                ans.push_back(i);
+            for (int i = start; i <= n; ++i)
+                ans.push_back(i);
+        }
+        
+        return ans;
+    }
+};
