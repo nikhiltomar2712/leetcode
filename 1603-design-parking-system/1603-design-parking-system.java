1faab3a9322c4729bca083a3e0556248b1e6d263
Nikhil Tomar <nikhiltomarsan2712@gmail.com>
2026-09-23 16:19:39 +0530

    Time: 7 ms (100%), Space: 47 MB (42.37%) - LeetHub

---
+++ 1603-design-parking-system/1603-design-parking-system.java
@@ -0,0 +1,25 @@
+class ParkingSystem {
+    private int big, medium, small;
+
+    public ParkingSystem(int big, int medium, int small) {
+        this.big = big;
+        this.medium = medium;
+        this.small = small;
+    }
+
+    public boolean addCar(int carType) {
+        switch (carType) {
+            case 1:
+                if (big > 0) { big--; return true; }
+                return false;
+            case 2:
+                if (medium > 0) { medium--; return true; }
+                return false;
+            case 3:
+                if (small > 0) { small--; return true; }
+                return false;
+            default:
+                return false;
+        }
+    }
+}
