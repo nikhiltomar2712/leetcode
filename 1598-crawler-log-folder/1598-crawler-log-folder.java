44719de880ddefb22b40888f75c1c065286c397f
Nikhil Tomar <nikhiltomarsan2712@gmail.com>
2026-09-23 16:13:34 +0530

    Time: 1 ms (100%), Space: 43.9 MB (44.35%) - LeetHub

---
+++ 1598-crawler-log-folder/1598-crawler-log-folder.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minOperations(String[] logs) {
+        int depth = 0;
+        for (String log : logs) {
+            if (log.equals("../")) {
+                if (depth > 0) depth--;
+            } else if (log.equals("./")) {
+                // stay
+            } else {
+                depth++;
+            }
+        }
+        return depth;
+    }
+}
